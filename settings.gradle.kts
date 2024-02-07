5dd22ad11cf4f62e963885576356834d058db0fe
Sven <68018116+SKBotNL@users.noreply.github.com>
2024-02-07 16:05:33 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,5 @@
-pluginManagement {                                          
-    repositories {
-        gradlePluginPortal()
-        google()
-    }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
 }
 
-plugins {                                                   
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
-
-rootProject.name = "Chat-OG"                           
-
-dependencyResolutionManagement {                            
-    repositories {
-        mavenCentral()
-    }
-}
+rootProject.name = "Chat-OG"
