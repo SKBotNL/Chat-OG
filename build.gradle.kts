162f85166abdf0e188a6b358415821b65d95dedb
NotAlexNoyle <alexnoyle@icloud.com>
2025-06-02 19:38:24 -0400

    Update shadow

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.io.BufferedReader
 
 plugins {
     kotlin("jvm") version "2.1.21"
-    id("com.gradleup.shadow") version "8.3.5"
+    id("com.gradleup.shadow") version "8.3.6"
     eclipse
 }
 
